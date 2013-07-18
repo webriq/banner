f0501a169aa644c3167f0bca616c8f6bc6183a87
David Pozsar <david.pozsar@megaweb.hu>
2013-07-18 11:46:59 +0200

    js & css improvements

--- public/scripts/zork/banner.js
+++ public/scripts/zork/banner.js
@@ -43,6 +43,91 @@
             "stop": function( event, ui ) {
                 ui.item.children( "legend" ).triggerHandler( "focusout" );
             }
+        },
+        addButtons = function ( element, isGlobal ) {
+            var templates = element.find( "> .type-templates" ),
+                addGroup  = function () {
+                    var group    = $( this ),
+                        header   = group.find( "> .banner-group-header:first" ),
+                        banners  = group.find( "> .banner-group-banners:first" ),
+                        addType  = $( "<select>" );
+
+                    if ( true || ! isGlobal ) { /// TODO: remove true
+                        header.prepend(
+                            $( "<button>" )
+                                .css( "float", "right" )
+                                .button( {
+                                    "text": false,
+                                    "icons": {
+                                        "primary": "ui-icon-trash"
+                                    }
+                                } )
+                                .click( function () {
+                                    group.remove();
+                                } )
+                        );
+                    }
+
+                    templates.each( function () {
+                        var template = $( this );
+
+                        addType.append(
+                            $( "<option>", {
+                                "value": template.data( "type" ),
+                                "text": js.core.translate( "banner.type." + template.data( "type" ) )
+                            } )
+                        );
+                    } );
+
+                    banners.prepend(
+                        $( "<div>" )
+                            .addClass( "banner-add" )
+                            .append( addType )
+                            .append(
+                                $( "<button>" )
+                                    .button( {
+                                        "text": false,
+                                        "icons": {
+                                            "primary": "ui-icon-plus"
+                                        }
+                                    } )
+                                    .click( function () {
+                                        var type     = addType.val(),
+                                            template = templates.filter( "[data-type='" + type + "']:first" );
+
+                                        banners.append(
+                                            $( template.data( "template" ) )
+                                                .each( addBanner )
+                                        );
+                                    } )
+                            )
+                    );
+                },
+                addBanner = function () {
+                    var banner   = $( this ),
+                        title    = banner.find( "> .banner-title:first" );
+
+                    title.prepend(
+                        $( "<button>" )
+                            .css( "float", "right" )
+                            .button( {
+                                "text": false,
+                                "icons": {
+                                    "primary": "ui-icon-trash"
+                                }
+                            } )
+                            .click( function () {
+                                banner.remove();
+                            } )
+                    );
+                };
+
+            element.find( "> .banner-group" )
+                   .each( addGroup )
+                   .find( "> .banner" )
+                   .each( addBanner );
+
+            return addGroup;
         };
 
     /**
@@ -55,6 +140,7 @@
         js.style( css );
         element = $( element );
         element.accordion( accordionParams );
+        addButtons( element, true );
     };
 
     global.Zork.Banner.prototype.globals.isElementConstructor = true;
