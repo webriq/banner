225dbd984c458f22d54b915b57351e7fada0bfc4
Gridguyz Dev Platform <dev-platform@gridguyz.com>
2013-12-09 02:22:18 +0000

    Translations updated

---
+++ languages/banner/hu.php
@@ -0,0 +1,7 @@
+<?php
+return array (
+  'banner.form.image.height' => 'Magasság',
+  'banner.form.image.width' => 'Szélesség',
+  'banner.form.set.name' => 'Név',
+  'banner.form.set.submit' => 'Mentés',
+);
